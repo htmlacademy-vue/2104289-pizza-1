072d8c2a64b073079acb161a3e74faa8962aa9e2
Danil <saqe789@gmail.com>
2022-05-11 13:38:01 +0300

    module 1

---
+++ src/frontend/src/views/Index.vue
@@ -0,0 +1,234 @@
+<template>
+  <main class="content">
+    <form action="#" method="post">
+      <div class="content__wrapper">
+        <h1 class="title title--big">Конструктор пиццы</h1>
+
+        <div class="content__dough">
+          <div class="sheet">
+            <h2 class="title title--small sheet__title">Выберите тесто</h2>
+
+            <div class="sheet__content dough">
+              <label
+                v-for="dough in pizza.dough"
+                :key="dough.id"
+                class="dough__input"
+                :class="'dough__input--' + isLight(dough.image)"
+              >
+                <input
+                  type="radio"
+                  name="dought"
+                  class="visually-hidden"
+                  :value="isLight(dough.image)"
+                />
+                <b>{{ dough.name }}</b>
+                <span>{{ dough.description }}</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div class="content__diameter">
+          <div class="sheet">
+            <h2 class="title title--small sheet__title">Выберите размер</h2>
+
+            <div class="sheet__content diameter">
+              <label
+                v-for="size in pizza.sizes"
+                class="diameter__input"
+                :class="'diameter__input--' + checkPizzaSize(size.multiplier)"
+                :key="size.id"
+              >
+                <input
+                  type="radio"
+                  name="diameter"
+                  :value="checkPizzaSize(size.multiplier)"
+                  class="visually-hidden"
+                />
+                <span>{{ size.name }}</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div class="content__ingredients">
+          <div class="sheet">
+            <h2 class="title title--small sheet__title">
+              Выберите ингредиенты
+            </h2>
+
+            <div class="sheet__content ingredients">
+              <div class="ingredients__sauce">
+                <p>Основной соус:</p>
+
+                <label
+                  v-for="sauce in pizza.sauces"
+                  :key="sauce.id"
+                  class="radio ingredients__input"
+                >
+                  <input
+                    type="radio"
+                    name="sauce"
+                    :value="sauce.name === 'Томатный' ? 'tomato' : 'creamy'"
+                    :checked="sauce.id === 1 && true"
+                  />
+                  <span>{{ sauce.name }}</span>
+                </label>
+              </div>
+
+              <div class="ingredients__filling">
+                <p>Начинка:</p>
+
+                <ul class="ingredients__list">
+                  <li
+                    v-for="ingredient in pizza.ingredients"
+                    :key="ingredient.id"
+                    class="ingredients__item"
+                  >
+                    <span
+                      class="filling"
+                      :class="'filling--' + getName(ingredient.image)"
+                      >{{ ingredient.name }}</span
+                    >
+
+                    <div class="counter counter--orange ingredients__counter">
+                      <button
+                        type="button"
+                        class="counter__button counter__button--minus"
+                        disabled
+                      >
+                        <span class="visually-hidden">Меньше</span>
+                      </button>
+                      <input
+                        type="text"
+                        name="counter"
+                        class="counter__input"
+                        value="0"
+                      />
+                      <button
+                        type="button"
+                        class="counter__button counter__button--plus"
+                      >
+                        <span class="visually-hidden">Больше</span>
+                      </button>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="content__pizza">
+          <label class="input">
+            <span class="visually-hidden">Название пиццы</span>
+            <input
+              type="text"
+              name="pizza_name"
+              placeholder="Введите название пиццы"
+            />
+          </label>
+
+          <div class="content__constructor">
+            <div class="pizza pizza--foundation--big-tomato">
+              <div class="pizza__wrapper">
+                <div class="pizza__filling pizza__filling--ananas"></div>
+                <div class="pizza__filling pizza__filling--bacon"></div>
+                <div class="pizza__filling pizza__filling--cheddar"></div>
+              </div>
+            </div>
+          </div>
+
+          <div class="content__result">
+            <p>Итого: 0 ₽</p>
+            <button type="button" class="button" disabled>Готовьте!</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </main>
+</template>
+
+<script>
+import misc from "@/static/misc.json";
+import pizza from "@/static/pizza.json";
+import user from "@/static/user.json";
+
+export default {
+  name: "Index",
+  data() {
+    return {
+      misc,
+      pizza,
+      user,
+      isLight: (item) => (item.includes("light") ? "light" : "large"),
+      checkPizzaSize(item) {
+        if (item === 1) {
+          return "small";
+        } else if (item === 2) {
+          return "normal";
+        } else {
+          return "big";
+        }
+      },
+      getName(item) {
+        const dot = item.indexOf(".");
+        const lastSlash = item.lastIndexOf("/");
+        return item.substring(lastSlash + 1, dot);
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+}
+
+.main__wrapper {
+  padding-bottom: 30px;
+
+  background-color: $white;
+  box-shadow: $shadow-light;
+
+  h1 {
+    margin-bottom: 0;
+    padding: 0 95px;
+
+    text-align: center;
+
+    @include b-s36-h42;
+  }
+
+  p {
+    padding: 0 95px;
+
+    text-align: center;
+
+    font-size: 20px;
+    line-height: 30px;
+  }
+
+  b {
+    font-size: 1.2em;
+  }
+}
+
+.main__header {
+  margin-bottom: 30px;
+  padding: 20px 0;
+
+  background-color: $green-600;
+
+  img {
+    display: block;
+
+    margin: 0 auto;
+  }
+}
+</style>
